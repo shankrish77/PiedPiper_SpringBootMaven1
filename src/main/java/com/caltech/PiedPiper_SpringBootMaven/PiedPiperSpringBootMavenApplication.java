ba3140fb33675a894c1be160da99b1f32e603e46
shankrish77 <shanthi_77@hotmail.com>
2021-04-25 19:37:42 -0400

    Added Java Maven web project Springboot files

---
+++ src/main/java/com/caltech/PiedPiper_SpringBootMaven/PiedPiperSpringBootMavenApplication.java
@@ -0,0 +1,13 @@
+package com.caltech.PiedPiper_SpringBootMaven;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PiedPiperSpringBootMavenApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PiedPiperSpringBootMavenApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/caltech/PiedPiper_SpringBootMaven/PiedPiperSpringBootMavenApplicationTests.java
@@ -0,0 +1,13 @@
+package com.caltech.PiedPiper_SpringBootMaven;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PiedPiperSpringBootMavenApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
