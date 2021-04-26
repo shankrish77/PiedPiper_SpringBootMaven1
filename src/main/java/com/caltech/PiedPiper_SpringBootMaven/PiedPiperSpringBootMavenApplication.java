b85c1c9dfe15c192e12afeb1835468f9876c6244
shankrish77 <74517850+shankrish77@users.noreply.github.com>
2021-04-26 09:39:20 -0700

    Update PiedPiperSpringBootMavenApplication.java

--- src/main/java/com/caltech/PiedPiper_SpringBootMaven/PiedPiperSpringBootMavenApplication.java
+++ src/main/java/com/caltech/PiedPiper_SpringBootMaven/PiedPiperSpringBootMavenApplication.java
@@ -11,3 +11,23 @@ public class PiedPiperSpringBootMavenApplication {
 	}
 
 }
+
+@RestController
+class GreetingController {
+    
+    @RequestMapping("/hello/{name}")
+    String hello(@PathVariable String name) {
+        return "Hello, " + name + "!";
+    }
+}
+
+@RestController
+public class TomcatController {
+
+    @GetMapping("/hello")
+    public Collection<String> sayHello() {
+        return IntStream.range(0, 10)
+          .mapToObj(i -> "Hello number " + i)
+          .collect(Collectors.toList());
+    }
+}
